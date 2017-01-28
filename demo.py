ae27201b9914351a54bb31cf9e96b4b8a81cfc4f
Stefan Veis Pennerup <stefanveispennerup@gmail.com>
2017-01-28 21:27:12 +0100

    Defined the NeuronLayer class

--- demo.py
+++ demo.py
@@ -2,6 +2,9 @@ __credits__ = ["Milo Spencer-Harper", "Siraj Raval"]
 
 from numpy import exp, array, random, dot
 
+class NeuronLayer():
+    def __init__(self, number_of_neurons, number_of_inputs_per_neuron):
+        self.synaptic_weights = 2 * random.random((number_of_inputs_per_neuron, number_of_neurons)) - 1
 
 class NeuralNetwork():
     def __init__(self):
